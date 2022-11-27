554fe0d3c1923d6aa0e9be026509a600afb1ecb3
Oleg Grytsynevych <oleggry@amazon.com>
2022-11-27 21:10:03 +0100

    Cleanup

--- Program.cs
+++ Program.cs
@@ -1,22 +1,19 @@
 ﻿using Akka.Actor;
 using reS3m;
 
-// TODO: add parameters
-// - no of workers
-// - chunk size
-// - Akka.NET <https://petabridge.com/blog/async-await-vs-pipeto/>
+// TODO: add CLI arguments
+// -w: no of workers
+// -c: chunk size // does it need to have alignment (e.g. like x1024)???
 
-var allWorkDone = new Barrier(2);
 var workers = 3;
 var chunkSize = 100;
-var buffer = new byte[chunkSize];
 
+using var allWorkDone = new Barrier(2);
 using var s3client = new Amazon.S3.AmazonS3Client();
 using var stdout = Console.OpenStandardOutput();
 
-var actorSystem = ActorSystem.Create("reS3m");
+using var actorSystem = ActorSystem.Create("reS3m");
 var manager = actorSystem.ActorOf(Props.Create<Manager>(s3client, stdout, workers, chunkSize, allWorkDone), "manager");
-
 
 var s3ObjName = Console.In.ReadLine();
 while(s3ObjName != null) {
@@ -30,12 +27,9 @@ while(s3ObjName != null) {
 
 manager.Tell(new reS3m.Messages.NoMoreWork());
 allWorkDone.SignalAndWait();
-//await actorSystem.Terminate();
 
 (string bucketName, string key) ParseS3Uri(string s3uri) {
     var uri = new System.Uri(s3uri);
     var key = uri.AbsolutePath.Substring(1); // remove 1st slash
     return (uri.Host, key);
 }
-
-
